c550f532cd32c6f07874a5fa145cc1e65121f52b
Drew Pate <andrewcpate@gmail.com>
2023-02-11 13:24:51 -0500

    adds basic structure and logic for the game, works on some styling and classNames

--- movie-roulette/src/App.jsx
+++ movie-roulette/src/App.jsx
@@ -2,11 +2,77 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState({
+    movieTitles: [
+      {
+        title: "",
+      },
+    ],
+  });
+
+  const [randomMovie, setRandomMovie] = useState([]);
+
+  const addMovie = () => {
+    const newMovie = {
+      title: "",
+    };
+    setMovies({
+      ...movies,
+      movieTitles: [...movies.movieTitles, newMovie],
+    });
+  };
+
+  const removeMovie = (index) => {
+    let data = { ...movies };
+    data.movieTitles.splice(index, 1);
+    setMovies({ ...movies });
+  };
+
+  const handleMovieChange = (e, index) => {
+    const updatedMovies = { ...movies };
+    updatedMovies.movieTitles[index][e.target.name] = e.target.value;
+    setMovies(updatedMovies);
+  };
+
+  const getRandom = () => {
+    let random =
+      movies.movieTitles[Math.floor(Math.random() * movies.movieTitles.length)];
+    console.log(random);
+    setRandomMovie(random);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("spinning!");
+    getRandom();
+  };
+
   return (
     <div className="App">
       <h1>Movie Roulette</h1>
-      <div>
-        <p>Inputfields go here</p>
+      <p>
+        Winner: <b>{randomMovie.title}</b>
+      </p>
+      <div className="input-container">
+        {movies.movieTitles.map((movie, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="text"
+                name="title"
+                id="movie"
+                label="Movie Title"
+                value={movie.title}
+                onChange={(e) => handleMovieChange(e, index)}
+              />
+              <button onClick={removeMovie}>Remove</button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="button-container">
+        <button onClick={handleSubmit}>Spin</button>
+        <button onClick={addMovie}>Add Movies</button>
       </div>
     </div>
   );
